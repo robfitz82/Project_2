999f2834a32f0fd72ad2eb1278ca976975a921c6
JamesDuffy <jaduffy007@gmail.com>
2014-11-19 14:53:15 -0800

    first commit access controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+
+  root 'access#landing'
+  
+  get 'login', to: "access#login", as: 'login'
+
+  get 'signup', to: "access#signup", as: 'signup'
+
+  post 'login', to: "access#attempt_login"
+
+  post 'signup', to: "access#create"
+
+  get 'home', to: "access#home", as: 'home'
+
+  get 'logout', to: "access#logout"
+
+  get '/forgot' => 'access#forgot_password'
+  post '/send_reset' => 'access#send_reset'
+  get '/reset/:token' => 'access#reset_password'
+  post '/update_password' => 'access#update_password'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
