466d8856167ea11712223baaf487b55a154b90eb
Myla Baker <myla.baker@gmail.com>
2014-11-20 14:21:32 -0800

    created method for seeding data with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 User.create({firstname: "John", lastname: "Doe", email: "test1@example.com", username: "user1", password: "password"})
 User.create({firstname: "Jane", lastname: "Doe", email: "test2@example.com", username: "user2", password: "password"})
 User.create({firstname: "Joe", lastname: "Testa", email: "test3@example.com", username: "user3", password: "password"})
@@ -15,3 +16,20 @@ Photo.create({title: "test1", width: 800 , height: 400, url: "http://lorempixel.
 Photo.create({title: "test2", width: 800 , height: 400, url: "http://lorempixel.com/800/400/city/", price: 250, user_id: 2})
 Photo.create({title: "test3", width: 800 , height: 400, url: "http://lorempixel.com/800/400/city/", price: 250, user_id: 3})
 Photo.create({title: "test4", width: 800 , height: 400, url: "http://lorempixel.com/800/400/city/", price: 250, user_id: 4})
+
+#Alternative
+
+# User.delete_all
+# 100.times do
+# 	fname = Faker::Name.first_name
+# 	lname = Faker::Name.last_name
+# 	User.create(
+# 		firstname: fname,
+# 		lastname: lname,
+# 		username: "#{fname}.#{lname}",
+# 		password: "password",
+# 		password_confirmation: "password",
+# 		email: "#{fname}.#{lname}@gmail.com"
+# 	) 
+# 	# create(:user)
+# end
